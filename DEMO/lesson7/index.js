feed62663d323b8afa2fecf6dbfb868c65c6ec13
Your Name <huy2261995@gmail.com>
2022-08-13 21:32:46 +0700

    1

---
+++ DEMO/lesson7/index.js
@@ -0,0 +1,23 @@
+// string
+let username='John';
+let email="john@gmail.com";
+console.log('username uppercase', username.toUpperCase());
+
+//number
+let a=1;
+let b=2;
+let c=a/b;
+console.log(c);
+
+//boolean
+let isHiddenMenu= true;
+
+//undefined
+let d;
+console.log('d',d);
+d=10;
+console.log('d2',d);
+
+//null
+let g=null;
+console.log(g);
