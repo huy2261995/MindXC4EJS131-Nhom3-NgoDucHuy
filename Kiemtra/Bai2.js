4fdf429f254a16251fc6b259ebb678d8edaf1717
huy2261995 <huy2261995@gmail.com>
2022-09-19 22:53:56 +0700

    kiemtra

--- Kiemtra/Bai2.js
+++ Kiemtra/Bai2.js
@@ -1,13 +1,22 @@
-function numberOneTriangle(n) {
-  if (n > 10) {
-    console.log("nhap i tu 1-10");
-  } else {
-    for (let i = 0; i < n; i++) {
-      let output = "";
-      for (let k = 0; k <= i; k++) output += "*";
-      console.log(output);
-    }
+// function numberOneTriangle(n) {
+//   if (n > 10) {
+//     console.log("nhap i tu 1-10");
+//   } else {
+//     for (let i = 0; i < n; i++) {
+//       let output = "";
+//       for (let k = 0; k <= i; k++) output += "*";
+//       console.log(output);
+//     }
+//   }
+// }
+
+// numberOneTriangle(3);
+
+function display(a) {
+  let b = "";
+  for (let i = 1; i <= a; i++) {
+    b = b.concat("*");
+    console.log(b);
   }
 }
-
-numberOneTriangle(3);
+display(12);
